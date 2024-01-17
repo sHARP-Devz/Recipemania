c07ca880a53b55029cdda451b626dab555d00b0e
harzyz <110671828+harzyz@users.noreply.github.com>
2024-01-17 12:34:27 +0100

    first frontend commit

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
