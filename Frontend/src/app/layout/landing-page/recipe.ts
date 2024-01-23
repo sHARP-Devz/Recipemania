bd9166ec3a974c1e5ed9b898d6fc87046069137e
Abadom Emeka <abadomemekavictor@gmail.com>
2024-01-23 17:08:11 +0100

    Emeka Landing Page

---
+++ Frontend/src/app/layout/landing-page/recipe.ts
@@ -0,0 +1,35 @@
+export const recipe_data = [
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    },
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    },
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    },
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    },
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    },
+    {
+        img: 'chicken.png',
+        recipe_name: 'Chicken',
+        user_name: 'Bambam'
+    }
+]
+
+
+
