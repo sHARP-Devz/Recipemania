b9bafb4a24b04bfe54666897af23d535ace9868f
Jaymie-Ivy21 <oluwajamie@gmail.com>
2024-01-31 05:05:48 +0100

    recipe page commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,12 @@ import {MatIconModule} from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
-<<<<<<< HEAD:Frontend/src/app/app.module.ts
-import { ModalComponent } from './shared/components/modal/modal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RecipePageComponent } from './layout/recipe-page/recipe-page.component';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalComponent } from './shared/components/modal/modal.component';
 import { LayoutComponent } from './layout/layout.component';
-import { MatIconModule } from '@angular/material/icon'
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LandingPageComponent } from './layout/landing-page/landing-page.component';
->>>>>>> 532273cd79ea44a86e725e36c75883ecfe92571e:src/app/app.module.ts
 
 @NgModule({
   declarations: [
@@ -25,14 +18,9 @@ import { LandingPageComponent } from './layout/landing-page/landing-page.compone
     LayoutComponent,
     AuthComponent,
     ModalComponent,
-<<<<<<< HEAD:Frontend/src/app/app.module.ts
-    RecipePageComponent
-
-=======
     LoginComponent,
     RegisterComponent,
     LandingPageComponent
->>>>>>> 532273cd79ea44a86e725e36c75883ecfe92571e:src/app/app.module.ts
   ],
   imports: [
     BrowserModule,
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+// import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
 
 
 @NgModule({
     declarations: [
-      LandingPageComponent
+      // LandingPageComponent,
+      RecipePageComponent
     ],
     imports: [
       MatIconModule
