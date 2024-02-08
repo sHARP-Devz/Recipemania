1adf3f387e27b6d714970f2b47db92a78bd478b4
harzyz <110671828+harzyz@users.noreply.github.com>
2024-02-08 05:27:52 +0100

    added title-block component

--- src/app/shared/components/title-block/title-block.component.ts
+++ src/app/shared/components/title-block/title-block.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-title-block',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterLink],
   templateUrl: './title-block.component.html',
   styleUrls: ['./title-block.component.css']
 })
