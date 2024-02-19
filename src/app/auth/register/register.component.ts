e9702849a4fa66b10def75d2ddaf26fe9bc70a59
harzyz <110671828+harzyz@users.noreply.github.com>
2024-02-19 11:55:01 +0100

    register fix

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
