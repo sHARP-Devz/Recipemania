1b1b1d9065761fd80844e92a01807dc77e6038d1
Abadom Emeka <abadomemekavictor@gmail.com>
2024-02-19 09:39:31 +0100

    navbar added

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
+import { ToggleService } from 'src/app/layouts/landing-page/toggle.service';
+import { MatIconModule } from '@angular/material/icon'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  @Input() backgroundColor: string | undefined;
+  @Input() textColor: string | undefined;
+  @Input() displayDiv: string | undefined;
+
+  // @Input() isLoggedIn: boolean = false; // Initially set to false to hide the navbar
+
+
+
+  isMobile: boolean = false;
+  toggle: boolean = false;
+
+
+  constructor(private toggleService: ToggleService) {
+  }
+
+  ngOnInit(): void {
+    this.toggleService.getIsMobile().subscribe(mobileView => {
+      this.isMobile = mobileView;
+    })
+
+    this.toggleService.getToggle().subscribe(toggleHamburger => {
+      this.toggle = toggleHamburger;
+    })
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.toggleService.onResize()
+  }
+
+  showMenu() {
+    if(this.isMobile) {
+      this.toggleService.toggleMenu()
+    }
+  }
+
+  
+  loginModalOpen: boolean = false
+  registerModalOpen: boolean = false
+
+
+  loginModaltoggle(open: boolean) {
+    this.loginModalOpen = open
+  }
+  registerModaltoggle(open: boolean) {
+    this.registerModalOpen = open
+  }
 
 }
